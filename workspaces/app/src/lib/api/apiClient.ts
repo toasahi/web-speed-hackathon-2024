fdf93cdee561857cd50a855d14e261bf1d3bcc04
toasahi <asa1313to@gmail.com>
2024-03-24 14:22:12 +0900

    Revert "axios をkyに変更"
    This reverts commit 7c27c12ab57e49e836930e2f369af7e2d9823b71.

--- workspaces/app/src/lib/api/apiClient.ts
+++ workspaces/app/src/lib/api/apiClient.ts
@@ -1,9 +1,24 @@
+// import axios from 'axios';
+
+// const createAxiosInstance = () => {
+//   const instance = axios.create({
+//     baseURL: process.env['API_URL'] || '/',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
+
+//   return instance;
+// };
+
+// export const apiClient = createAxiosInstance();
+
 import ky from 'ky';
 
 const createKyInstance = () => {
   const instance = ky.create({
     credentials: 'include',
-    prefixUrl: process.env['API_URL'] || '/',
+    prefixUrl: process.env['API_URL'] || '',
     headers: {
       'Content-Type': 'application/json',
     },
